81524aa06acb6cb8dd7b3361f1674dec31e09fae
jmickelin <jmickelin@3ab695d0-b58c-11dd-9501-15ef518ea1c2>
2008-11-28 23:08:39 +0000

    Possible speed increase by use of early binding.

--- trunk/fft/test.py
+++ trunk/fft/test.py
@@ -3,13 +3,13 @@ import pydft
 #from pylab import plot, show
 
 a = []
-for i in range(1024):
+for i in range(128):
     a.append(math.sin(2*math.pi*i/128))
 #print "a = ", a
  
 b = pydft.DFT(a)
 
-#print b
+print b
 
 #plot(b)
 #show()
