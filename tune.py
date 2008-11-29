b61e37d3f49d019037af92b7f660a8101ce7f760
jmickelin <jmickelin@3ab695d0-b58c-11dd-9501-15ef518ea1c2>
2008-11-29 05:58:54 +0000

    Added more stuff, among other things a recursive, moving average filter in the time domain (did I already say that?). Also, I cleaned up the code a bit.

--- tune.py
+++ tune.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # Tune guitar
+# (c) Jonne Mickelin 2008
 # -*- coding: utf-8 -*-
 
 import math
 import pyaudio
 import struct
 import time
-#import matplotlib.pyplot as plt
+
 import pylab
 
 import fft.pydft as dft
@@ -48,20 +49,10 @@ def tone_freq(num):
     
     return 2**(num/12.0) * 440.0
 
-# def dft(vals):
-#     N = len(vals)
-#     freqs = []
-#     for i in xrange(N):
-#         R = 0
-#         I = 0
-#         for j in xrange(N):
-#             R += vals[j]*math.cos(-2*math.pi*i*j/N)
-#             I -= vals[j]*math.sin(-2*math.pi*i*j/N)
-#         freqs.append((R, I))
-
-#     return freqs
-
 def disect(data):
+    """Return an array of Python values, by unpacking the given C-data 
+    using the global variable struct_format as a format identifier."""
+
     size = struct.calcsize(struct_format)
     datas = []
     for i in xrange(chunk):
@@ -83,28 +74,29 @@ def f_filter(data):
     #data[0] = 0
     return data
 
-def t_filter(data):
-    f = 512
-    a = [] 
+def t_filter(data, f=512):
+    """Filter out noise in the time domain. Argument is the frequency of 
+    the noise."""
 
+    filtered = [] 
+
+    # We use a recursice moving average filter
     for i in xrange(len(data)):
         try:
-            a.append(a[i-1] + data[i] - data[i-f])
-        except IndexError:
-            a.append(data[i])
-    #for i in range(len(data)):
-    #    b = 0
-    #    for j in range(i - f, i):
-    #        try:
-    #            b += data[j]
-    #        except IndexError:
-    #            pass
-    #    a.append(b/f)
-
-    return a
+            filtered.append(filtered[i-1] + data[i] - data[i-f])   # Last value, off with the value f steps ago, 
+                                                                   # on with a new value.
+        except IndexError:    
+            filtered.append(data[i])   # Now, this is probably wrong
+                                       # What we do is we set the filtered data to be 
+                                       # equal to the unfiltered data for the first f
+                                       # samples.
+    return filtered
     
 
 def freq(data):
+    """Deprecated. Convert data to python objects, then perform 
+    the DFT. It also returned the index of the frequency with the 
+    highest amplitude at some point."""
     disected = disect(data)
     freqs = dft.DFT(disected)
     #return maxindex(freqs)
@@ -114,36 +106,57 @@ def main():
     for num, name in enumerate(notes):
         print "%s\t: %.2f Hz" % (name, tone_freq(num-len(notes)+3))
  
+    # Open the interface to the Mic
     p = pyaudio.PyAudio()
-    
     instream = p.open(format=FORMAT,
                       channels=CHANNELS,
                       rate=RATE,
                       input=True, # It is, indeed, an input stream
                       frames_per_buffer=chunk)
 
-    
-    #data = instream.read(chunk*5) # Read some data and throw it away. Not sure this is necessary
+    # Make matplotlib interactive. It says "interactive", at least, but I don't see
+    # any buttons. This makes a window appear without blocking the application
+    # (which pylab.show() would have done).
+    pylab.ion()
+
+    # Read data one time first
     data = instream.read(chunk)
     d = t_filter(disect(data))
     f = dft.DFT(d)
 
-    pylab.ion()
+    # Set up the two subplots and make their scales fixed
+    ax1 = pylab.subplot(211)
+    ax1.set_autoscale_on(False)
+    ax1.set_xlim(xmin=-10, xmax=len(d)+10)  # We pad the graph on the sides so we can
+                                            # see better
+    ax1.set_ylim((-max(d)*100, max(d)*100)) # Starts out with just noise, hopefully. 
+                                            # This should be set manually to match the 
+                                            # expectedmaximum amplitude, but I'm not 
+                                            # sure what level that is.
+                                            #The noise seems to be around 70000
+
+    ax2 = pylab.subplot(212)
+    ax2.set_autoscale_on(False)
+    ax2.set_xlim(xmin=-10, xmax=len(f)+10)
+    ax2.set_ylim((0, max(f)))
+
+    # Plot the preliminary data, so that we may use set_ydata for animation later
     n = range(len(f))
-    pylab.subplot(211)
-    line, = pylab.plot(n, d) #http://www.scipy.org/Cookbook/Matplotlib/Animations
-    pylab.subplot(212)
-    line2, = pylab.plot(n, f) 
+    line1, = ax1.plot(n, d) #http://www.scipy.org/Cookbook/Matplotlib/Animations
+    line2, = ax2.plot(n, f) 
 
     while True:
-        tstart = time.time()
-        data = instream.read(chunk)
-        d = t_filter(disect(data))
-        f = dft.DFT(d)
-        line.set_ydata(d)
-        line2.set_ydata(f_filter(f))
-        pylab.draw()
-        #time.sleep(0.1)
+        try:
+            data = instream.read(chunk) # Read data from the mic
+            d = disect(data)  # Convert this data to values that python can understand
+            d = t_filter(d)   # Filter data in the time domain to remove noise
+            f = dft.DFT(d)    # Perform the DFT on the filtered data
+            line1.set_ydata(d) # Update the plots
+            line2.set_ydata(f)
+            pylab.draw()       # Draw it, and then repeat
+        except KeyboardInterrupt:
+            print "C-c was pressed. Exiting."
+            break
 
 if __name__ == "__main__":
     main()
