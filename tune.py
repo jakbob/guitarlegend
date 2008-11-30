5cb7300c2039418e63857149adfae03d0dcbd4bc
jmickelin <jmickelin@3ab695d0-b58c-11dd-9501-15ef518ea1c2>
2008-11-30 22:03:42 +0000

    Hm, now, if we use pylab's FFT, the filter works perfectly. However, with our DFT it doesn't. Shouldn't they yield the same results?

--- tune.py
+++ tune.py
@@ -37,17 +37,20 @@ notes = [ u"C",
           u"a\u266f",
           u"h" ]
 
-chunk = 1024               # samples per buffer, i.e. number of samples to fetch every time read is called
+chunk = 4000               # samples per buffer, i.e. number of samples to fetch every time read is called
 FORMAT = pyaudio.paInt16   # 16 bits per sample
 struct_format = "1h"       # same as above
 CHANNELS = 1               # mono
-RATE = 44100               # Hz, samples per second
+RATE = 22050               # Hz, samples per second
 #RECORD_SECONDS = 5
 
 def tone_freq(num):
     """Return the frequency for the tone number num, where 0 is the middle A (440 Hz)"""
     
     return 2**(num/12.0) * 440.0
+
+def freq_tone(freq):
+    return log(f/440.0)/log(2) * 12
 
 def disect(data):
     """Return an array of Python values, by unpacking the given C-data 
@@ -74,24 +77,46 @@ def f_filter(data):
     #data[0] = 0
     return data
 
-def t_filter(data, f=512):
+def t_filter(data, N=512):
     """Filter out noise in the time domain. Argument is the frequency of 
     the noise."""
 
+    def hejsum(d, a, b):
+        if a < 0:
+            a = 0
+        return sum(d[a:b])
+
     filtered = []
 
-    if f == 0:
+    if N == 0:
         return data
 
-    for i in xrange(len(data)):
-        if i-f < 0: rem = 0
-        else: rem = data[i-f]
-        if i < 1: last = 0
-        else: last = filtered[i-1]
-        
-        w = last - (rem + data[i])/f
-        filtered.append(w)
+    #for i in xrange(len(data)): # Recursive definition. Broken.
+    #    if i-f < 0: rem = 0
+    #    else: rem = data[i-f]
+    #    if i < 1: last = 0
+    #    else: last = filtered[i-1]
+    #    
+    #    w = last - (rem + data[i])
+    #    filtered.append(w)
+    
+    i = 0
+    while i < len(data):
+        a = hejsum(data, i-N, i)
+        filtered.append(a/(N+1))
+        i += 1
 
+    # for i in range(len(data)):
+    #         b = 0
+    #         for j in range(i-f, i):
+    #             try: 
+    #                 b += data[j]
+    #             except IndexError:
+    #                 pass
+    #         filtered.append(b/f)
+        
+    #     print "kuk"
+    
     return filtered
 
 #     filtered = [] 
@@ -143,10 +168,31 @@ def average(l1, l2):
     
     return r
 
-def multiply(data, magnitude=1000):
+def multiply(data, fun):
     for i in range(len(data)):
-        data[i] *= 0.1
+        data[i] *= fun(i)
     return data
+
+def hamming(n):
+    return 0.53836 - 0.46164*math.cos(2*math.pi*n/(chunk-1))
+
+def highpass(i, n):
+    if i < n:
+        return 0
+    else: 
+        return 1
+
+def bandpass(i, a, b):
+    if a <= i <= b:
+        return 1
+    else:
+        return 0
+
+def make_highpass(n):
+    return lambda n: highpass(n, n)
+
+def make_bandpass(a, b):
+    return lambda n: bandpass(n, a, b)
 
 def main():
     for num, name in enumerate(notes):
@@ -167,7 +213,8 @@ def main():
 
     # Read data one time first
     data = instream.read(chunk)
-    d = t_filter(disect(data))
+    d = disect(data)
+    #d = t_filter(d)
     f1 = dft.DFT(d)
 
     # Set up the two subplots and make their scales fixed
@@ -175,7 +222,7 @@ def main():
     ax1.set_autoscale_on(False)
     ax1.set_xlim(xmin=-10, xmax=len(d)+10)  # We pad the graph on the sides so we can
                                             # see better
-    ax1.set_ylim((-200000, 200000))             # Starts out with just noise, hopefully. 
+    ax1.set_ylim((-30000, 30000))             # Starts out with just noise, hopefully. 
                                             # This should be set manually to match the 
                                             # expectedmaximum amplitude, but I'm not 
                                             # sure what level that is.
@@ -183,23 +230,25 @@ def main():
 
     ax2 = pylab.subplot(212)
     ax2.set_autoscale_on(False)
-    ax2.set_xlim(xmin=-10, xmax=len(f1)+10)
+    ax2.set_xlim(xmin=-100, xmax=RATE+100)
     ax2.set_ylim((0, max(f1)/100))
+    ax2.set_xlabel("Hz")
 
     # Plot the preliminary data, so that we may use set_ydata for animation later
-    n = range(len(f1))
+    n = range(len(d))
+    n2 = pylab.arange(0, RATE, float(RATE)/chunk)
     line1, = ax1.plot(n, d) #http://www.scipy.org/Cookbook/Matplotlib/Animations
-    line2, = ax2.plot(n, f1) 
+    line2, = ax2.plot(n2, f1) 
 
     while True:
         try:
             data = instream.read(chunk) # Read data from the mic
             d = disect(data)            # Convert this data to values that python can understand
-            d = t_filter(d, 512)        # Filter data in the time domain to remove noise
+            d = t_filter(d, 52)        # Filter data in the time domain to remove noise
 
-            #d = multiply(d)
-
-            f2 = dft.DFT(d)             # Perform the DFT on the filtered data
+            #f2 = dft.DFT(d)             # Perform the DFT on the filtered data
+            f2 = pylab.fft(d)
+            #f2 = multiply(f2, make_bandpass(20.0/RATE, 20000.0/RATE))
 
             #f2 = average(f2, f1)        # Okay, I don't know if this is an established 
                                         # method, but it's an attempt att reducing fluctuations
