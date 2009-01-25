7c2b387cbc7a38919dfc22e5334578c61fffcb34
jakob@cp80.se <jakob@cp80.se@3ab695d0-b58c-11dd-9501-15ef518ea1c2>
2009-01-25 17:32:16 +0000

    det är inte klart, men jag behöver spara det jag gjort

--- trunk/graphics.py
+++ trunk/graphics.py
@@ -6,5 +6,25 @@
 #
 # (c) Jonne Mickelin & Jakob Florell 2008-09
 
+import pyglet
+
 import error
+
+circle = pyglet.imgage.load("data/circle.bmp") #ja, det är ett hack, jag pallarnte
+straight = pyglet.imgage.load("data/straight.bmp")
+
+
+class DeathNote:
+	def __init__(self, note, ticksPerQuarter):
+		quarters = float(note.stop-note.start)/ticksPerQuarter
+		width = int(quarters*40) #magiskt nummer... jag pallarinte. Längden på en fjärdedelsnot
+		img = pyglet.image.AbstractImage(width, circle.height)
+		img.blit_into(circle, 0,0,0)
+		if width>=80: #80=40*2 jag borde verkligen sluta med det här.
+			for x in xrange((width-80)/straight.width):
+				img.blit_into(straight, x*straight.width,0,0)
+			img.blit_into(circle, width-circle.width,0,0)#det här är under *host* konstruktion
+			
+			
+		
 
--- trunk/tab.py
+++ trunk/tab.py
@@ -9,58 +9,62 @@
 import midi
 
 class Note:
-  def __init__(self, pitch, string, start, stop):
-        print string
+	"""Symbolises a note. Which string to play and what fret."""
+	def __init__(self, pitch, string, start, stop):
+		print string
 
-        self.pitch=pitch
-        self.string=string
-        self.start=start
-        self.stop=stop
-        
-        if string==1:
-            base = 64
-        elif string==2:
-            base = 59
-        elif string==3:
-            base = 55
-        elif string==4:
-            base = 50
-        elif string==5:
-            base = 45
-        elif string==6:
-            base = 40
-        else:
-            print "fräs"
-        self.fret=self.pitch-base
-    
+		self.pitch=pitch
+		self.string=string
+		self.start=start
+		self.stop=stop
+		
+		if string==1: #guitar string nr 1=high e
+			base = 64 #midi number for high e
+		elif string==2: #b
+			base = 59
+		elif string==3: #g
+			base = 55
+		elif string==4: #d
+			base = 50
+		elif string==5: #a
+			base = 45
+		elif string==6: #E
+			base = 40
+			
+		self.fret=self.pitch-base
+	
   def __repr__(self):
-      return "<NOTE! String: " + str(self.string+1) + " Fret: " + str(self.fret) + " Start: " + str(self.start) + " Stop: " + str(self.stop) + ">"
+	  return "<NOTE! String: " + str(self.string+1) + " Fret: " + str(self.fret) + " Start: " + str(self.start) + " Stop: " + str(self.stop) + ">"
 
 class Tab:
-    def __init__(self, midifile):
-        f=midi.MidiFile()
-        f.open(midifile)
-        f.read()
-        f.close()
-        
-        self.string=[[],[],[],[],[],[],]
-        #for t in xrange(1,7):
-        for track in f.tracks:
-            startevent=None
-            #try: #litesmahackigt. menmen
-                #f.tracks[i]
-            #except IndexError:
-                #break
-            for event in track.events:
-                if event.channel>6 or event.velocity==0: #
-                  continue #else
-                if not startevent:
-                    if event.type=="NOTE_ON":
-                        startevent=event
-                else:
-                    if (event.type=="NOTE_OFF" and startevent.pitch==event.pitch)\
-                    or event.type=="NOTE_ON":
-                        self.string[event.channel-1].append(Note(startevent.pitch, startevent.channel, startevent.time, event.time))
-                        startevent=None
-                    if event.type=="NOTE_ON":
-                        startevent=event
+	"""Opens up a midifile and converts all midi events into Notes"""
+	def __init__(self, midifile):
+		f=midi.MidiFile()
+		f.open(midifile)
+		f.read()
+		f.close()
+		
+		self.ticksPerSec = f.ticksPerSecond
+		self.ticksPerQuarter = f.ticksPerQuarter
+		self.string=[[],[],[],[],[],[],]
+		#for t in xrange(1,7):
+		for track in f.tracks:
+			startevent=None
+			#try: #litesmahackigt. menmen
+				#f.tracks[i]
+			#except IndexError:
+				#break
+			for event in track.events:
+				if event.channel<1 or event.channel>6 or event.velocity==0: #these events shuoldn't be in the file, but if they are, skip them.
+				  continue #else
+				if not startevent:
+					if event.type=="NOTE_ON":
+						startevent=event
+				else:
+					if (event.type=="NOTE_OFF" and startevent.pitch==event.pitch)\ #when the corresponding NOTe_OFF event shows up, make the note
+					or event.type=="NOTE_ON": #or if we get a new note on message before. This isn't supposed to happen so I implemented this as a sequrity messure.
+						self.string[event.channel-1].append(Note(startevent.pitch, startevent.channel, startevent.time, event.time))
+						startevent=None
+					if event.type=="NOTE_ON": #If we got a new NOTE_ON before the NOTE_OFF stop the old note and start a new
+						startevent=event
+
