235814bb15e71a6a7d631af7fec30925fa77a6d7
jakob <jakob@cp80.se>
2009-01-25 17:32:16 +0000

    det är inte klart, men jag behöver spara det jag gjort

---
+++ slask/particlesystem.py
@@ -0,0 +1,19 @@
+﻿import pyglet
+import random
+
+class Particle:
+	def __init__(self, active, life, fadetime, pos, vel):
+		self.active = active
+		self.life = life
+		self.fade = fadetime
+		self.pos = pos
+		self.vel = vel
+		
+		self.gravity = (0,0,0) #to be implemented
+
+class ParticleSystem:
+	def __init__(self, many):
+		self.particles = []
+		for i in xrange(many):
+			self.particles.append(Particle(True, 100.0, random.random(), (100,100,100), (0,0,0)))
+		
