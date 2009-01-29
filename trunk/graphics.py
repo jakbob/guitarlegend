ed7b531e6a96c7e57fa91840a2b2cc92efd57751
fikobjakon <fikobjakon@3ab695d0-b58c-11dd-9501-15ef518ea1c2>
2009-01-29 22:56:39 +0000

    Ansiktslyftning+nytt dynamiskt laddsystem som inte fungerar så bra. Preloada blir nog bäst i alla fall...

--- trunk/graphics.py
+++ trunk/graphics.py
@@ -21,10 +21,14 @@ class DeathNote:
     def __init__(self, note, ticksPerQuarter, batch=None):
         self.note = note
         quarters = float(note.stop-note.start)/ticksPerQuarter
-        width = int(quarters*quarterlen) #magiskt nummer... jag pallarinte. L�ngden p� en fj�rdedelsnot
+        self.width = int(quarters*quarterlen) #bättre lösning här?
+        self.sprite = None
+        self.label = None
+        self.batch = batch
+        self.textured = False #this note haven't been given a texture yet
+    
+    def _get_texture(self, width):
         img = pyglet.image.Texture.create(width, start_circle.height)
-
-
         img.blit_into(end_circle, width-end_circle.width,0,0)
         img.blit_into(start_circle, 0,0,0)
         
@@ -35,11 +39,26 @@ class DeathNote:
             rest = width-(2*start_circle.width+(x+1)*straight.width)
             if rest>0:
                 img.blit_into(straight.get_region(0,0,rest,straight.height), start_circle.width+(x+1)*straight.width,0,0)
-
-        self.sprite = pyglet.sprite.Sprite(img, batch=batch)	
-        
-        self.sprite.color = (40*self.note.string+7*self.note.fret,30*self.note.string+5*self.note.fret,35*self.note.string+2*self.note.fret)
+        self.sprite = pyglet.sprite.Sprite(img, x=self.x, y=self.y, batch=self.batch)   
+        self.sprite.color = (40*self.note.string+12*self.note.fret,30*self.note.string+15*self.note.fret,35*self.note.string+17*self.note.fret)
+        self.label = pyglet.text.Label(str(self.note.fret),font_size=30,color=(0,0,0,220),x=self.x,y=self.y,anchor_x="center",anchor_y="center",batch=self.batch)
+        self.textured = True
     
+    def update(self):
+        if self.textured:
+            self.sprite.x = self.x
+            self.label.x = self.x+30
+            self.sprite.y = self.y
+            self.label.y = self.y+self.sprite.height/2
+        if self.x < 640*2 and not self.textured: #hur kommer man åt window.heigth härifrån?
+            self._get_texture(self.width)
+        if self.x < -640 and self.textured:
+            self.sprite.delete() #för säkerhets skull
+            del self.sprite #eventuellt döda hela objektet?
+            del self.label
+            self.textured = False
+            print "nu e den död"
+        
 if __name__ == "__main__":
     import tab
     window = pyglet.window.Window()
@@ -50,5 +69,6 @@ if __name__ == "__main__":
     def on_draw():
         window.clear()
         olle.sprite.draw()
+        olle.label.draw()
 
     pyglet.app.run()
