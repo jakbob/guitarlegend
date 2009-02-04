7d0f074c4b4ff54965792c3a20c6f5cb68040563
jmickelin <jmickelin@3ab695d0-b58c-11dd-9501-15ef518ea1c2>
2009-02-04 14:55:35 +0000

    Does not quite work yet.

--- fft/dft.c
+++ fft/dft.c
@@ -1,3 +1,4 @@
+
 /* dft.c
  * Provide functions for the Discrete Fourier transform (O(n^2) time).
  *
@@ -62,22 +63,79 @@ complex* DFT(float * data_points, int N)
   return frequencies;
 }
 
+/* Arguments: 
+   data -- real valued time domain input (double*)
+   N    -- length of data (int)
+*/
+complex *
+FFT(double* data, int N)
+{
+  int i, j;
+  double temp;
+  
+  // Shuffle input
+  for (i = 0; i < N/2; i+=2)
+    {
+      temp = data[i+1];              // Swap the values of the odd indexed 
+      data[i+1] = data[i+N/2];       // data points so that i - (i+1) == N/2 
+      data[i+N/2] = temp;            // for all even i.
+    }                                
+  
+  // Do the FFT
+  for (i = 1; i < N; i<<=1)
+    {
+      printf("%i:\n", i);
+      
+      // Loop over each data point and do the summations
+      for (j = 0; j < N; j+=2)
+	{
+	  printf("\t%.0lf + %.0lf => %i, %i\n", data[j], data[(j+i)%N], j, (j+i)%N);
+	  printf("\t%.0lf + %.0lf => %i, %i\n", data[(j+i)%N], data[j], (j+i)%N, j);
+	  //printf("%.0lf + %.0lf => %i\n", data[(j+1)%N], data[j], (j+i)%N);
+	}
+      printf("\n");
+      
+    }
+  
+  /*for (i = N; i > 1; i>>=1)
+    {
+      for (j = 0; j < N; j++)
+	{
+	  printf("%i, %i\n", i, i+j);
+	}
+    }
+  */
+  
+  // Print the result
+  /*for (i = 0; i < N; i++)
+    {
+      printf("%lf\n", data[i]);
+    }
+
+    return data;*/
+}
+
 #ifdef DEBUG
+#define NUM 8
 int main()
 {
-  float data[1024];
+  double data[NUM];
   complex* freqs;
   int t;
   
-  for (t = 0; t < 1024; t++)
+  /*for (t = 0; t < 1024; t++)
     {
       data[t] = sin(2*PI*t/1024) + sin(2*PI * 2 * t/1024) - sin(2*PI * 5 * t/1024);
     }
-  
-  freqs = DFT(data, 1024);
+  */
+  for (t = 0; t < NUM; t++)
+    {
+      data[t] = t+1;
+    }
+  freqs = FFT(data, NUM);
 
   // Ok, I put in some stuff for easy printing and plotting with python
-  printf("import pylab");
+  /*printf("import pylab");
   
   printf("#*** Time domain ***\n");
   
@@ -101,7 +159,7 @@ int main()
   printf("pylab.subplot(212)\npylab.bar(t, frequencydomain)\n\n");
   
   printf("pylab.show()\n\n");
-
+  */
   return 0;
 }
 #endif
