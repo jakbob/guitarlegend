d3924cfabadcc8d00120e1c5ddd6aac2cad9c41c
jmickelin <jmickelin@3ab695d0-b58c-11dd-9501-15ef518ea1c2>
2009-02-11 11:06:51 +0000

    Changed api a bit, fixed jakob's player in TestinNotes

--- trunk/soundtestscene.py
+++ trunk/soundtestscene.py
@@ -25,9 +25,9 @@ import pylab
 import scene
 import options
 
-plot_lock = multiprocessing.RLock()
+#plot_lock = multiprocessing.RLock()
 
-# def begin_plotting(scene):
+#def begin_plotting(scene):
 #     setup_plot(scene)
 #     update_plot(scene)
 
@@ -88,6 +88,31 @@ plot_lock = multiprocessing.RLock()
         
 #             time.sleep(0.1)
 
+class SoundBarfingProcess(multiprocessing.Process):
+    def __init__(self, 
+                 format=options.INPUT_FORMAT, 
+                 channels=options.INPUT_CHANNELS, 
+                 rate=options.INPUT_RATE, 
+                 frames_per_buffer=options.INPUT_CHUNK_SIZE):
+
+        multiprocessing.Process.__init__(self)
+        
+        self.name = "Sound test"
+
+        # We put everything that has to do with pyaudio in it's
+        # own thread to avoid undefined behaviour.
+        p = pyaudio.PyAudio()
+        self.instream = p.open(format=format,
+                               channels=channels,
+                               rate=rate,
+                               input=True,      # It is, indeed, an input stream
+                               frames_per_buffer=frames_per_buffer)
+        self.quit = False
+
+    def run(self):
+        while not self.quit:
+            print "borka"
+
 
 class SoundTestScene(scene.TestScene):
     
@@ -100,41 +125,21 @@ class SoundTestScene(scene.TestScene):
                  channels=options.INPUT_CHANNELS, 
                  rate=options.INPUT_RATE, 
                  frames_per_buffer=options.INPUT_CHUNK_SIZE):
-        
+        self.soundthread = SoundBarfingProcess(format, channels, 
+                                               rate, frames_per_buffer)
+
         self.name = "Sound test"
+        
+        self.soundthread.start()
 
-        p = pyaudio.PyAudio()
-        self.instream = p.open(format=format,
-                               channels=channels,
-                               rate=rate,
-                               input=True,             # It is, indeed, an input stream
-                               frames_per_buffer=frames_per_buffer)
-        #self._setup_plot()
-
-        #def _setup_plot(self):
-        #setup_plot(self)
-        #self.update_plot_thread = threading.Thread(target=self.update_plot)
-        #self.update_plot_thread = multiprocessing.Process(target=begin_plotting, args=(self,))
-        self.quit = False
-        #self.update_plot_thread.start()
-        #self.update_plot()
-
-        #pyglet.clock.schedule_interval(self.update_plot, 1) # Still too slow...
-
-    def __del__(self):
-        #pyglet.clock.unschedule(self.update_plot)
-        self.quit = True
-        self.update_plot_thread.join()
+    #def __del__(self):
+    #    self.soundthread.quit = True
+    #    self.soundthread.join()
+    def end(self):
+        self.soundthread.quit = True
 
     def debug_draw(self, window):
-        #ax = fig.gca()
-        #ax.plot(y)
-        # Update the data
         window.clear()
-        # Platt
-        #with self.plot_lock:
-        #with plot_lock:
-        #self.pyglet_plot.blit(0,0)
 
     def do_logic(self, dt):
         pass
