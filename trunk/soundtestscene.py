8a26068fbc68654a9e8584822cb5703580619c30
jmickelin <jmickelin@3ab695d0-b58c-11dd-9501-15ef518ea1c2>
2009-02-11 14:11:36 +0000

    Began on the FFT scene. It just prints the FFT so far, but it's working.

--- trunk/soundtestscene.py
+++ trunk/soundtestscene.py
@@ -4,8 +4,20 @@
 from __future__ import with_statement
 
 #import threading
-import multiprocessing
+try:
+    import multiprocessing
+except ImportError, err:
+    try:
+        import pyprocessing as multiprocessing
+    except ImportError:
+        message = "You appear to be using Python 2.5 or older,\
+and do not appear to have pyprocessing installed. Either update to\
+Python 2.6 or install pyprocessing from http://pyprocessing.berlios.de/"
+        error.critical(message)
+        error.bail_out(err)
+
 import time
+import struct
 
 ####################
 # Required modules #
@@ -24,72 +36,11 @@ import pylab
 ####################
 import scene
 import options
-
-#plot_lock = multiprocessing.RLock()
-
-#def begin_plotting(scene):
-#     setup_plot(scene)
-#     update_plot(scene)
-
-# def setup_plot(thescene):
-#     pylab.ion()  # Makes pylab interactive. Plotting does not blockthe application.
-    
-#     # The figure lets us define the physical size of the plot, so that we can create a texture from it
-#     thescene.graph_dpi = 50
-#     thescene.graph_width = 4
-#     thescene.graph_height = 4
-    
-#     thescene.fig = pylab.figure(figsize=[thescene.graph_width, thescene.graph_height], # Inches
-#                                 dpi=thescene.graph_dpi,        # 100 dots per inch, so the resulting buffer is 400x400 pixels
-#                                 )
-    
-#     # Set up the axes for plotting
-#     thescene.time_plane = pylab.subplot(211)
-#     thescene.freq_plane = pylab.subplot(212)
-    
-#     thescene.fig.add_axes(thescene.time_plane)
-#     thescene.fig.add_axes(thescene.freq_plane)
-    
-#     # Do not change the scale to match the graph
-#     thescene.time_plane.set_autoscale_on(False) 
-#     thescene.freq_plane.set_autoscale_on(False)
-    
-#     # Set the scales of the plot. TODO They use magic numbers. Fix this.
-#     thescene.time_plane.set_xlim(xmin=-10, xmax=options.INPUT_CHUNK_SIZE + 10)
-#     thescene.time_plane.set_ylim((0, 20000))
-    
-#     thescene.freq_plane.set_xlim(xmin=-100, xmax=options.INPUT_CHUNK_SIZE-10000)
-#     thescene.freq_plane.set_ylim((0, 100000))
-    
-#     time_x = range(options.INPUT_CHUNK_SIZE)
-#     freq_x = pylab.arange(0, options.INPUT_RATE, step=float(options.INPUT_RATE)/options.INPUT_CHUNK_SIZE)
-    
-#     # Set up initial data, so that we get instances of the plots' data,
-#     # that we can edit
-#     thescene.time_data, = thescene.time_plane.plot(time_x, [0]*options.INPUT_CHUNK_SIZE)
-#     thescene.freq_data, = thescene.freq_plane.plot(freq_x, [0]*options.INPUT_CHUNK_SIZE)
-    
-#     thescene.pyglet_plot = pyglet.image.create(thescene.graph_dpi * thescene.graph_width, 
-#                                                thescene.graph_dpi * thescene.graph_width)
-
-# def update_plot(scene):
-#         while not scene.quit:
-#             canvas = agg.FigureCanvasAgg(scene.fig)
-#             canvas.draw()
-#             renderer = canvas.get_renderer()
-#             scene.raw_data = renderer.tostring_argb() # Why isn't rgb and pitch=-3*400 below working?
-
-#             raw_pyglet_image = scene.pyglet_plot.get_image_data()
-
-#             print "Setting"
-#             with plot_lock:
-#                 raw_pyglet_image.set_data("ARGB", -4*(scene.graph_width*scene.graph_dpi),
-#                                           scene.raw_data)
-        
-#             time.sleep(0.1)
+import fft
 
 class SoundBarfingProcess(multiprocessing.Process):
     def __init__(self, 
+                 quit_pipe,
                  format=options.INPUT_FORMAT, 
                  channels=options.INPUT_CHANNELS, 
                  rate=options.INPUT_RATE, 
@@ -108,11 +59,30 @@ class SoundBarfingProcess(multiprocessing.Process):
                                input=True,      # It is, indeed, an input stream
                                frames_per_buffer=frames_per_buffer)
         self.quit = False
+        self.quit_pipe = quit_pipe
 
+        self.chunk_size = frames_per_buffer
+
+    def disect(self, data):
+        """Return an array of Python values, by unpacking the given C-data 
+        using the global variable struct_format as a format identifier."""
+        
+        size = struct.calcsize(options.STRUCT_INPUT_FORMAT)
+        datas = []
+        for i in xrange(self.chunk_size):
+            datas.append(struct.unpack(options.STRUCT_INPUT_FORMAT, data[i:i+size])[0])
+        return datas
+ 
     def run(self):
         while not self.quit:
-            print "borka"
-
+            if self.quit_pipe.poll():
+                self.quit = self.quit_pipe.recv()
+                print "Recieved quit instruction. Value:", self.quit
+            else:
+                raw_data = self.instream.read(self.chunk_size)
+                disected = self.disect(raw_data)
+                freqs = fft.FFT(disected)
+                print freqs
 
 class SoundTestScene(scene.TestScene):
     
@@ -125,18 +95,21 @@ class SoundTestScene(scene.TestScene):
                  channels=options.INPUT_CHANNELS, 
                  rate=options.INPUT_RATE, 
                  frames_per_buffer=options.INPUT_CHUNK_SIZE):
-        self.soundthread = SoundBarfingProcess(format, channels, 
+
+        # Use this to communicate quit events
+        self.quit_pipe, child_quit_pipe = multiprocessing.Pipe()
+
+        self.soundthread = SoundBarfingProcess(child_quit_pipe, 
+                                               format, channels, 
                                                rate, frames_per_buffer)
 
         self.name = "Sound test"
         
         self.soundthread.start()
 
-    #def __del__(self):
-    #    self.soundthread.quit = True
-    #    self.soundthread.join()
     def end(self):
-        self.soundthread.quit = True
+         self.quit_pipe.send(True)
+         self.soundthread.join()
 
     def debug_draw(self, window):
         window.clear()
