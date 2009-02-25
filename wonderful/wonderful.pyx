21038ac7bbe2a04eb06ebd1832342eccc1340f22
jmickelin <jmickelin@3ab695d0-b58c-11dd-9501-15ef518ea1c2>
2009-02-25 12:54:28 +0000

    Bajses

---
+++ wonderful/wonderful.pyx
@@ -0,0 +1,30 @@
+cdef extern from "math.h":
+    double sqrt(double)
+    double tan(double)
+
+cdef extern from "stdlib.h":
+    ctypedef int size_t
+    ctypedef long intptr_t
+    void * malloc(size_t size)
+    void free(void * ptr)
+
+cdef struct complex:
+    double re
+    double im
+
+cdef extern from "portaudio.h":
+    ctypedef struct PaStream
+
+cdef extern from "wonderful.h":
+    cdef struct ring_buffer:
+        complex * data
+    
+        unsigned int size
+        unsigned int consume_index
+        unsigned int write_index
+
+    ctypedef struct inputData:
+        struct ring_buffer * samples
+
+    int wonderful_init(inputData * data, PaStream * stream)
+    int wonderful_terminate(inputData * data, PaStream * stream)
