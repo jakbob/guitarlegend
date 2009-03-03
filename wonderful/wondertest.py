9b6bf017f5c3790062fc511cdb2a32b2db6017ef
jmickelin <jmickelin@3ab695d0-b58c-11dd-9501-15ef518ea1c2>
2009-03-03 13:08:23 +0000

    Cellopung

--- wonderful/wondertest.py
+++ wonderful/wondertest.py
@@ -8,13 +8,15 @@ wonderful.init(SAMPLE_RATE, N)
 
 lasttime = time.clock()
 
+lowest_hearable = int(20 * N/float(SAMPLE_RATE))
+
 try:
     while True:
         freqs = wonderful.munch()
         if freqs is not None:
-            largest = heapq.nlargest(6, enumerate(freqs), key=(lambda (num, amp): amp))
+            largest = heapq.nlargest(6, enumerate(freqs[lowest_hearable:N/2]), key=(lambda (num, amp): amp))
             t = time.clock()
-            print " ".join(["%.3f" % (p*float(SAMPLE_RATE)/N) for (p, mag) in largest]),
+            print " ".join(["%.3f" % ((p+lowest_hearable)*float(SAMPLE_RATE)/N) for (p, mag) in largest if mag > 100]),
             print " Idle time:", t-lasttime
             lasttime = t
             #print " ".join(["%.3f" % mag for (p, mag) in largest])
